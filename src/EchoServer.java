d94b134cde62e7a333237b190fd6c1b634451861
Michał Marasek <mimatek22@gmail.com>
2017-11-17 17:43:06 +0100

    Dodanie zawartosci podstawowej apki czat

---
+++ src/EchoServer.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+ 
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+ 
+/** 
+ * @ServerEndpoint gives the relative name for the end point
+ * This will be accessed via ws://localhost:8080/EchoChamber/echo
+ * Where "localhost" is the address of the host,
+ * "EchoChamber" is the name of the package
+ * and "echo" is the address to access this class from the server
+ */
+@ServerEndpoint("/echo/{roomID}") 
+public class EchoServer {
+    
+    SessionHandler handler = new SessionHandler();
+    /**
+     * @OnOpen allows us to intercept the creation of a new session.
+     * The session class allows us to send data to the user.
+     * In the method onOpen, we'll let the user know that the handshake was 
+     * successful.
+     */
+    @OnOpen
+    public void onOpen(Session session, @PathParam("roomID") final String roomID){
+        System.out.println(session.getId() + " has opened a connection"); 
+        session.getUserProperties().put("roomID", roomID);
+        handler.addSession(session);
+        try {
+            session.getBasicRemote().sendText("Connection Established");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+ 
+    /**
+     * When a user sends a message to the server, this method will intercept the message
+     * and allow us to react to it. For now the message is read as a String.
+     */
+    @OnMessage
+    public void onMessage(String message, Session session){
+        System.out.println("Message from " + session.getId() + ": " + message);
+//        handler.sendToAllConnectedSessions(message);
+        if(session.getUserProperties().get("roomID").toString().length() > 0){
+            handler.sendToAllConnectedSessionsInRoom(session.getUserProperties().get("roomID").toString(), message);
+        }else{
+            handler.sendToAllConnectedSessions(message);
+        }
+    }
+ 
+    /**
+     * The user closes the connection.
+     * 
+     * Note: you can't send messages to the client from this method
+     */
+    @OnClose
+    public void onClose(Session session){
+        System.out.println("Session " +session.getId()+" has ended");
+    }
+}
